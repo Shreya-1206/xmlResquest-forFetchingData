b5d81684a9766be62247e915a0add9daa6cb4103
Shreya-1206 <131991284+Shreya-1206@users.noreply.github.com>
2023-09-25 13:35:14 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // Import stylesheets
 import './style.css';
 
-const app = document.querySelector('#app');
+const joke = document.querySelector('#joke');
+const refreshBtn = document.querySelector('#refreshBtn');
 const uri = 'https://icanhazdadjoke.com/';
 
 const xhr = new XMLHttpRequest(); ///created our xhr obj
 xhr.addEventListener('load', function () {
-  app.innerHTML = `<h1>${this.responseText}</h1>`;
+  joke.innerHTML = `<h1>${this.responseText}</h1>`;
 });
 
-xhr.open('GET', uri); //we have configured our xhr obj
-xhr.setRequestHeader('Accept', 'text/plain'); //seting the accept header after the opening of xhr
-xhr.send();
+refreshBtn.addEventListener('click', function () {
+  xhr.open('GET', uri); //we have configured our xhr obj
+  xhr.setRequestHeader('Accept', 'text/plain'); //seting the accept header after the opening of xhr
+  xhr.send();
+});
